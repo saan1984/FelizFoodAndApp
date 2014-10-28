f0755ade8ce6f5d685f83b461767693f3c916046
sandeep <sandeep.kumar.patel@sap.com>
2014-10-28 14:27:32 +0530

    Initial commit

---
+++ src/com/feliz/food/sensor/FelizActivity.java
@@ -0,0 +1,58 @@
+package com.feliz.food.sensor;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.webkit.HttpAuthHandler;
+import android.webkit.SslErrorHandler;
+import android.net.http.SslError;
+import org.apache.commons.codec.binary.Base64;
+import java.util.Map;
+import java.util.HashMap;
+
+
+
+public class FelizActivity extends Activity {
+
+    private WebView webView = null;
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+        webView = (WebView) findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url);
+                return false;
+            }
+            @Override
+            public void onReceivedHttpAuthRequest(WebView view,
+                                                  HttpAuthHandler handler, String host, String realm) {
+                handler.proceed("SYSTEM", "Abcd1234");
+            }
+
+            public void onReceivedSslError(WebView view,
+                                           SslErrorHandler handler,
+                                           SslError error) {
+                handler.proceed() ;
+            }
+        });
+
+        String up = "SYSTEM" +":" +"Abcd1234";
+        String authEncoded = new String(Base64.encodeBase64(up.getBytes()));
+        String authHeader = "Basic " +authEncoded;
+        Map<String, String> headers = new HashMap<String, String>();
+        headers.put("Authorization", authHeader);
+
+
+       webView.loadUrl("http://blri4w1012.blrl.sap.corp:8000/D3FoodSensor/index.html#/",headers);
+    }
+}
